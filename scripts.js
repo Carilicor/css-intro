e26673b28ff85045753a7e3800b1f145b4b5b416
Cari Licor <Carilicor@gmail.com>
2022-10-24 15:50:53 -0400

    updated css

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const family = [{
+    dad: true,
+    name: 'sue',
+    job: 'painter',
+}, {
+    kid1: 'cherry',
+    pet: 'fish',
+}
+]
+console.log("I'm the boss!!")
+
+document.getElementById('contact').style.color = 'yellow'
+
+const handleColorChange = () => {
+    console.log('changing color')
+}
+
+const handleAlert = () => {
+    alert(' Alerting the class')
+}
